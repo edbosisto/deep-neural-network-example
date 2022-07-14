9b149761a341530d3e2ec518bb5e297b4a8b1767
edbosisto <e.bosisto@gmail.com>
2022-07-14 12:31:33 +0200

    linear_activation_backward function created

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import numpy as np
 
 
-# equations for computation
+# Equations for computation
 def sigmoid(x):
     z = np.exp(-x)
     sig = 1 / (1 + z)
@@ -209,6 +209,34 @@ def linear_backward(dZ, cache):
     dW = (1 / m) * np.dot(dZ, A_prev.T)
     db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
     dA_prev = np.dot(W.T, dZ)
+
+    return dA_prev, dW, db
+
+
+# Linear activation - backward
+def linear_activation_backward(dA, cache, activation):
+    """
+    Implement the backward propagation for the LINEAR->ACTIVATION layer.
+
+    Arguments:
+    dA -- post-activation gradient for current layer l
+    cache -- tuple of values (linear_cache, activation_cache) we store for computing backward propagation efficiently
+    activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
+
+    Returns:
+    dA_prev -- Gradient of the cost with respect to the activation (of the previous layer l-1), same shape as A_prev
+    dW -- Gradient of the cost with respect to W (current layer l), same shape as W
+    db -- Gradient of the cost with respect to b (current layer l), same shape as b
+    """
+    linear_cache, activation_cache = cache
+
+    if activation == "relu":
+        dZ = relu_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+    elif activation == "sigmoid":
+        dZ = sigmoid_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
 
     return dA_prev, dW, db
 
