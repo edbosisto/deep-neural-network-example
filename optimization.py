92de08e248bfa5be6d62934a476eb013ec6b13e0
edbosisto <e.bosisto@gmail.com>
2022-07-21 13:36:43 +0200

    added Model_learn_decay() which can use various optimization methods as well as learning_rate_decay

--- optimization.py
+++ optimization.py
@@ -357,3 +357,101 @@ def model(X, Y, layers_dims, optimizer, learning_rate=0.0007, mini_batch_size=64
 
     return parameters
 
+
+#####################################################################
+# MODEL WHICH CAN USE LEARNING RATE DECAY (AND OTHER OPTIMIZATIONS) #
+#####################################################################
+
+def model_learn_decay(X, Y, layers_dims, optimizer, learning_rate=0.0007, mini_batch_size=64, beta=0.9,
+          beta1=0.9, beta2=0.999, epsilon=1e-8, num_epochs=5000, print_cost=True, decay=None, decay_rate=1):
+    """
+    3-layer neural network model which can be run in different optimizer modes.
+
+    Arguments:
+    X -- input data, of shape (2, number of examples)
+    Y -- true "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
+    layers_dims -- python list, containing the size of each layer
+    learning_rate -- the learning rate, scalar.
+    mini_batch_size -- the size of a mini batch
+    beta -- Momentum hyperparameter
+    beta1 -- Exponential decay hyperparameter for the past gradients estimates
+    beta2 -- Exponential decay hyperparameter for the past squared gradients estimates
+    epsilon -- hyperparameter preventing division by zero in Adam updates
+    num_epochs -- number of epochs
+    print_cost -- True to print the cost every 1000 epochs
+
+    Returns:
+    parameters -- python dictionary containing your updated parameters
+    """
+
+    L = len(layers_dims)  # number of layers in the neural networks
+    costs = []  # to keep track of the cost
+    t = 0  # initializing the counter required for Adam update
+    seed = 10  # For grading purposes, so that your "random" minibatches are the same as ours
+    m = X.shape[1]  # number of training examples
+    lr_rates = []
+    learning_rate0 = learning_rate  # the original learning rate
+
+    # Initialize parameters
+    parameters = initialize_parameters(layers_dims)  # function found in nn_models.py
+
+    # Initialize the optimizer
+    if optimizer == "gd":
+        pass  # no initialization required for gradient descent
+    elif optimizer == "momentum":
+        v = initialize_velocity(parameters)
+    elif optimizer == "adam":
+        v, s = initialize_adam(parameters)
+
+    # Optimization loop
+    for i in range(num_epochs):
+
+        # Define the random minibatches. We increment the seed to reshuffle differently the dataset after each epoch
+        seed = seed + 1
+        minibatches = random_mini_batches(X, Y, mini_batch_size, seed)
+        cost_total = 0
+
+        for minibatch in minibatches:
+
+            # Select a minibatch
+            (minibatch_X, minibatch_Y) = minibatch
+
+            # Forward propagation
+            a3, caches = forward_propagation(minibatch_X, parameters)  # function found in nn_models.py
+
+            # Compute cost and add to the cost total
+            cost_total += compute_cost(a3, minibatch_Y)  # function found in nn_models.py
+
+            # Backward propagation
+            grads = backward_propagation(minibatch_X, minibatch_Y, caches)  # function found in nn_models.py
+
+            # Update parameters
+            if optimizer == "gd":
+                parameters = update_parameters_with_gd(parameters, grads, learning_rate)
+            elif optimizer == "momentum":
+                parameters, v = update_parameters_with_momentum(parameters, grads, v, beta, learning_rate)
+            elif optimizer == "adam":
+                t = t + 1  # Adam counter
+                parameters, v, s, _, _ = update_parameters_with_adam(parameters, grads, v, s,
+                                                                     t, learning_rate, beta1, beta2, epsilon)
+        cost_avg = cost_total / m
+        if decay:
+            learning_rate = decay(learning_rate0, i, decay_rate)
+        # Print the cost every 1000 epoch
+        if print_cost and i % 1000 == 0:
+            print("Cost after epoch %i: %f" % (i, cost_avg))
+            if decay:
+                print("learning rate after epoch %i: %f" % (i, learning_rate))
+        if print_cost and i % 100 == 0:
+            costs.append(cost_avg)
+
+    # plot the cost
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('epochs (per 100)')
+    plt.title("Learning rate = " + str(learning_rate))
+    plt.show()
+
+    return parameters
+
+
