dc12c02d1140194261ff5e53eeb34e8ce0e65828
edbosisto <e.bosisto@gmail.com>
2022-07-21 13:38:30 +0200

    functions for learning rate decay and scheduled learning rate decay added

--- optimization.py
+++ optimization.py
@@ -455,3 +455,42 @@ def model_learn_decay(X, Y, layers_dims, optimizer, learning_rate=0.0007, mini_b
     return parameters
 
 
+# Update learning_rate (For learning rate decay)
+
+def update_lr(learning_rate0, epoch_num, decay_rate):
+    """
+    Calculates updated the learning rate using exponential weight decay.
+
+    Arguments:
+    learning_rate0 -- Original learning rate. Scalar
+    epoch_num -- Epoch number. Integer
+    decay_rate -- Decay rate. Scalar
+
+    Returns:
+    learning_rate -- Updated learning rate. Scalar
+    """
+
+    learning_rate = (1 / (1 + decay_rate * epoch_num)) * learning_rate0
+
+    return learning_rate
+
+
+# Scheduled learning rate decay
+
+def schedule_lr_decay(learning_rate0, epoch_num, decay_rate, time_interval=1000):
+    """
+    Calculates updated the learning rate using exponential weight decay.
+
+    Arguments:
+    learning_rate0 -- Original learning rate. Scalar
+    epoch_num -- Epoch number. Integer.
+    decay_rate -- Decay rate. Scalar.
+    time_interval -- Number of epochs where you update the learning rate.
+
+    Returns:
+    learning_rate -- Updated learning rate. Scalar
+    """
+
+    learning_rate = (1 / (1 + decay_rate * math.floor(epoch_num / time_interval)) * learning_rate0)
+
+    return learning_rate
