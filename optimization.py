a27a062019ed5e718b46b09d9f9d22656b2e4635
edbosisto <e.bosisto@gmail.com>
2022-07-21 13:22:46 +0200

    Gradient descent - update parameters function

--- optimization.py
+++ optimization.py
@@ -3,3 +3,39 @@ Functions for optimization of multilayered neural networks
 """
 
 
+####################
+# GRADIENT DESCENT #
+####################
+
+# Update parameters with Gradient Descent
+
+def update_parameters_with_gd(parameters, grads, learning_rate):
+    """
+    Update parameters using one step of gradient descent
+
+    Arguments:
+    parameters -- python dictionary containing your parameters to be updated:
+                    parameters['W' + str(l)] = Wl
+                    parameters['b' + str(l)] = bl
+    grads -- python dictionary containing your gradients to update each parameters:
+                    grads['dW' + str(l)] = dWl
+                    grads['db' + str(l)] = dbl
+    learning_rate -- the learning rate, scalar.
+
+    Returns:
+    parameters -- python dictionary containing your updated parameters
+    """
+    L = len(parameters) // 2  # number of layers in the neural networks
+
+    # Update rule for each parameter
+    for l in range(1, L + 1):
+        parameters["W" + str(l)] = parameters["W" + str(l)] - learning_rate * grads["dW" + str(l)]
+        parameters["b" + str(l)] = parameters["b" + str(l)] - learning_rate * grads["db" + str(l)]
+    return parameters
+
+
+######################
+# MINI BATCH DESCENT #
+######################
+
+
